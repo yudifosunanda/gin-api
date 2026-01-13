7f825d7df61c2439cd363ff03fa54c05bda9cc7a
unknown <sunandadifo@gmail.com>
2026-01-13 15:48:00 +0700

    feat! : update gorm to v2

--- db/database.go
+++ db/database.go
@@ -3,11 +3,11 @@ package db
 import (
 	"fmt"
 	"log"
-	"github.com/jinzhu/gorm"
-	_ "github.com/lib/pq" // PostgreSQL driver
 	"os"
-  "github.com/joho/godotenv"
-	
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 // DB is the global variable that will hold the database connection
@@ -19,26 +19,19 @@ func InitDB() {
 
 	errEnv := godotenv.Load()
 
-	if errEnv != nil{
+	if errEnv != nil {
 		fmt.Println("no env found")
 	}
 
-  host := os.Getenv("DB_HOST")
-  dbName := os.Getenv("DB_NAME")
-  dbPassword := os.Getenv("DB_PASSWORD")
-	
+	host := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
 	// Adjust the connection string as per your database setup
 	connStr := fmt.Sprintf("user=postgres dbname=%s password=%s host=%s sslmode=disable", dbName, dbPassword, host)
-	DB, err = gorm.Open("postgres", connStr)
+	DB, err = gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
 	fmt.Println("Database connected successfully!")
-}
-
-// CloseDB closes the database connection when the application exits
-func CloseDB() {
-	if DB != nil {
-		DB.Close()
-	}
 }
